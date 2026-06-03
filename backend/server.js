6bb28cdaa5a6c5137d6e6ffdd4865028dc58aa9c
Ayush Jha <aayushjha49@example.com>
2026-06-04 00:56:45 +0545

    4- prepared the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -21,9 +22,10 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve()
 
-app.use(express.json({limit: "10mb"})); // to parse req.body
-app.use(express.urlencoded({extended: true})); // to parse form data
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
 
@@ -33,7 +35,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+    app.get("*splat", (req,res) => {  // ← only this line changed
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 
 app.listen(PORT, ()=>{
